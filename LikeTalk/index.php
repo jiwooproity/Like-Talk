53329c8241f7ebad4d6e9221a76c966c6d6b938d
jiwooproity <jiwooproity@naver.com>
2021-12-08 23:29:33 +0900

    첫번째 채팅 앱 가입 페이지 50% 완성 / LikeTalk

---
+++ LikeTalk/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>LikeTalk</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <section class="form__signup">
+                <header class="header">
+                    <span class="logo">LIKETALK</span>
+                    <div class="mac">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </header>
+                <div class="header__bottom">
+                    <div class="header__bottom__link">
+                        <div class="header__bottom__detail">
+                            <span>https://www.liketalk.co.kr/</span><span>register_main</span>
+                        </div>
+                    </div>
+                </div>
+                <form class="form">
+                    <div class="field">
+                        <label>First Name</label>
+                        <input type="text" placeholder="당신의 성을 입력해주세요."/>
+                    </div>
+                    <div class="field">
+                        <label>Last Name</label>
+                        <input type="text" placeholder="당신의 이름을 입력해주세요."/>
+                    </div>
+                    <div class="field">
+                        <label>Password</label>
+                        <input type="password" placeholder="당신의 비밀번호를 입력해주세요."/>
+                    </div>
+                    <div class="field">
+                        <label>Password Confirm</label>
+                        <input type="text" placeholder="비밀번호를 한번 더 입력해주세요."/>
+                    </div>
+                    <div class="field">
+                        <label>Email</label>
+                        <input type="text" placeholder="당신의 이메일을 입력해주세요."/>
+                    </div>
+                    <div class="field">
+                        <label>Select Image</label>
+                        <input type="text" placeholder="당신의 이메일을 입력해주세요."/>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </body>
+</html>
